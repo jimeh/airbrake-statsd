c3300d264cd82f6d2e4d179ec115d56a7f758b49
Jim Myhrberg <contact@jimeh.me>
2012-02-03 18:38:38 +0000

    hook into the airbrake gem in a cleaner and simpler way

--- lib/airbrake-statsd/airbrake_ext.rb
+++ lib/airbrake-statsd/airbrake_ext.rb
@@ -1,21 +1,15 @@
 module Airbrake
   class << self
 
-    def notify_with_statds(*args)
+    private
+
+    def send_notice_with_statsd(*args)
       Airbrake::Statsd.increment
-      notify_without_statsd(*args)
+      send_notice_without_statsd(*args)
     end
 
-    alias :notify_without_statsd :notify
-    alias :notify :notify_with_statds
-
-    def notify_or_ignore_with_statsd(*args)
-      Airbrake::Statsd.increment
-      notify_or_ignore_without_statsd(*args)
-    end
-
-    alias :notify_or_ignore_without_statsd :notify_or_ignore
-    alias :notify_or_ignore :notify_or_ignore_with_statsd
+    alias :send_notice_without_statsd :send_notice
+    alias :send_notice :send_notice_with_statsd
 
   end
 end
--- spec/unit/airbrake-statsd/airbrake_ext_spec.rb
+++ spec/unit/airbrake-statsd/airbrake_ext_spec.rb
@@ -4,58 +4,33 @@ describe Airbrake do
 
   subject { Airbrake }
 
-  it '#notify is an alias to #notify_with_statsd' do
-    subject.method(:notify).should == subject.method(:notify_with_statds)
+  let(:notice) { mock('Notice', :to_xml => '') }
+  let(:configuration) { mock('Configuration', :public? => true) }
+
+  let(:sender) do
+    sender = mock('Sender')
+    sender.stub(:send_to_airbrake)
+    sender
   end
 
-  it '#notify_or_ignore is an alias to #notify_or_ignore_with_statsd' do
-    subject.method(:notify_or_ignore).
-      should == subject.method(:notify_or_ignore_with_statsd)
-  end
+  describe '#send_notice_without_statsd' do
+    before do
+      Airbrake.stub(:configuration).and_return(configuration)
+      Airbrake.stub(:sender).and_return(sender)
+    end
 
-  describe '#notify_without_statsd' do
     it 'does not call Airbrake::Statsd.increment' do
-      Airbrake.stub(:send_notice).and_return(nil)
-
-      Airbrake.should_receive(:build_notice_for).with('oops', {})
       Airbrake::Statsd.should_not_receive(:increment)
-
-      Airbrake.notify_without_statsd('oops', {})
+      Airbrake.send(:send_notice_without_statsd, notice)
     end
   end
 
-  describe '#notify' do
+  describe '#send_notice' do
     it 'calls Airbrake::Statsd.increment' do
-      Airbrake.stub(:send_notice).and_return(nil)
-
-      Airbrake.should_receive(:build_notice_for).with('oops', {})
       Airbrake::Statsd.should_receive(:increment)
+      Airbrake.should_receive(:send_notice_without_statsd)
 
-      Airbrake.notify('oops', {})
-    end
-  end
-
-  describe '#notify_or_ignore_without_statsd' do
-    it 'does not call Airbrake::Statsd.increment' do
-      Airbrake.stub(:send_notice).and_return(nil)
-
-      Airbrake.should_receive(:build_notice_for).with('oops', {}).
-        and_return(mock('Notice', :ignore? => false))
-      Airbrake::Statsd.should_not_receive(:increment)
-
-      Airbrake.notify_or_ignore_without_statsd('oops', {})
-    end
-  end
-
-  describe '#notify_or_ignore' do
-    it 'calls Airbrake::Statsd.increment' do
-      Airbrake.stub(:send_notice).and_return(nil)
-
-      Airbrake.should_receive(:build_notice_for).with('oops', {}).
-        and_return(mock('Notice', :ignore? => false))
-      Airbrake::Statsd.should_receive(:increment)
-
-      Airbrake.notify_or_ignore('oops', {})
+      Airbrake.send(:send_notice, notice)
     end
   end
 
